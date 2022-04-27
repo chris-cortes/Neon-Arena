f88ea1d2b32e016bee0e8b1d6e3d34c2566c64f6
c-cortex <cortes.d.chris@gmail.com>
2022-04-27 04:16:24 -0700

    Added particle vfx for bullet impact

--- Neon Arena/Assets/Scripts/Gun.cs
+++ Neon Arena/Assets/Scripts/Gun.cs
@@ -1,3 +1,5 @@
+// https://www.youtube.com/watch?v=THnivyG0Mvo
+
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -6,6 +8,7 @@ public class Gun : MonoBehaviour
     public float range = 100f;
 
     public Camera fpsCam;
+    public GameObject impactEffect;
 
     // Update is called once per frame
     void Update()
@@ -22,6 +25,9 @@ public class Gun : MonoBehaviour
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
+
+            GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGo, 1f);
         }
     }
 }
