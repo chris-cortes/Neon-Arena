8ed557dbc4826437a1fb985ea7d984e0dfa533aa
Nathaniel Mason <nmason0204@gmail.com>
2022-06-01 03:10:57 -0700

    Timer High Score Working Now

--- Neon Arena/Assets/Scripts/Scene1_Score.cs
+++ Neon Arena/Assets/Scripts/Scene1_Score.cs
@@ -5,18 +5,20 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 //got help from: https://answers.unity.com/questions/1173303/how-to-check-which-scene-is-loaded-and-write-if-co.html
+//got seconds help from: https://answers.unity.com/questions/1038757/how-to-make-a-timer-that-counts-up-in-seconds-as-a.html
 
 public class Scene1_Score : MonoBehaviour
 {
     public Text menuScore;
     public Text timerTextValue;
-    private float seconds;
-    static float s1Score = 0;
+    private float timer;
+    private int seconds;
+    static int s1Score = 0;
     string sceneName;
 
     void Start()
     {
-        seconds = 0f;
+        timer = 0f;
         Scene currentScene = SceneManager.GetActiveScene();
 
         sceneName = currentScene.name;
@@ -32,28 +34,31 @@ public class Scene1_Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(s1Score);
 
         if(sceneName == "Map1")
         {
-            seconds += Time.deltaTime;
+            //Debug.Log(s1Score);
+            timer += Time.deltaTime;
+            seconds = (int)timer % 60;
             timerTextValue.text = "" + (seconds);
+
+            if (seconds > s1Score)
+            {
+                s1Score = seconds;
+            }
         }
         
 
-        if(seconds > s1Score)
+        if (sceneName == "StartMenu")
         {
-            s1Score = seconds;
-
-            if(sceneName == "StartMenu")
+            //Debug.Log("In Start Menu");
+            string s1ScoreStr = s1Score.ToString();
+            if (menuScore.text != s1ScoreStr)
             {
-                string s1ScoreStr = s1Score.ToString();
-                if(menuScore.text != s1ScoreStr)
-                {
-                    menuScore.text = s1ScoreStr;
-                }
-                
+                menuScore.text = s1ScoreStr;
+                //Debug.Log("UPDATED SCORE");
             }
+
         }
     }
 }
